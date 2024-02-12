fc8d53b12fa8cc2bb959efda6766f571c11c3fdd
Tanmay Jadav <tanmayjadav21@gmail.com>
2024-02-12 14:31:57 +0530

    Commited

---
+++ src/components/Github.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+const Github = () => {
+    const data = useLoaderData()
+    // const [data,setData] = useState([])
+
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/tanmayjadav')
+    //     .then(response => response.json() )
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // },[])
+
+  return (
+    <div className='flex p-10 bg-slate-500 text-center text-xl bold'>
+      <img src= {data.avatar_url} alt="" width={300}/>
+      <span className='flex p-10 justify-center text-center'>Github Followers : {data.id}</span>
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () =>{
+    const response = await fetch('https://api.github.com/users/tanmayjadav')
+    return response.json( )
+}
